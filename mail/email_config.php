784f2c959b8c5aa7820b44b6706823f7db9052d1
hieund220604 <163495850+hieund220604@users.noreply.github.com>
2024-08-21 23:49:09 +0700

    Add files via upload

---
+++ mail/email_config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config['email'] = Array(
+    'protocol' => 'smtp',
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => 587,
+    'smtp_user' => 'hieundgch220990@fpt.edu.vn',
+    'smtp_fullname' => 'Customer Service',
+    'smtp_pass' => 'kphj sjqf csxt ljfk',
+    'smtp_secure' => 'tls',
+    'smtp_timeout' => '7',
+    'mailtype' => 'html',
+    'charset' => 'UTF-8'
+);
+?>
